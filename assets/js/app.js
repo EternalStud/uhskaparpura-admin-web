07c25c866ca879a08dc7a892f89adcbe2ccb52b7
786Eternal <mdzafarali.ali@gmail.com>
2026-07-11 10:14:04 +0530

    Add missing mobile-workspace-container styling with default display: none on desktop, and bump version to v1.2.4

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import { initRouter, navigateTo } from "./router.js?v=1.2.3";
+import { initRouter, navigateTo } from "./router.js?v=1.2.4";
 import { restoreSession } from "../../services/session.js";
 import { showToast } from "../../components/toast.js";
 
