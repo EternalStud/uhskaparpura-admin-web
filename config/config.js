f5a39aef0c198d08cdcdcd841eecd03edf5eba47
786Eternal <mdzafarali.ali@gmail.com>
2026-07-20 02:43:29 +0530

    deploy: update API_BASE_URL to version 104 with optimized backend performance

--- config/config.js
+++ config/config.js
@@ -2,5 +2,5 @@
 
 export const CONFIG = Object.freeze({
   GOOGLE_CLIENT_ID: "430143608351-ped4h40m0k35dqhhrgsptiri7rjpp23v.apps.googleusercontent.com",
-  API_BASE_URL: "https://script.google.com/macros/s/AKfycbyhhOW12LuQhroFQEywpX-fz6-WRG6wT-_OmD4T0kstYccIxVT7nn-MwiKniERkh_jD/exec",
+  API_BASE_URL: "https://script.google.com/macros/s/AKfycbwj-Y-b0rxV5HrxWynqHJSNzIaXwX89_h4kaEuBATFJ8RgUvOaRaAZ9f_lqORT25vw/exec",
 })
